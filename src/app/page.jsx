24af42e20eb1b6849b7e750bb948fe341266c0d4
narindra20 <hei.narindra@gmail.com>
2026-04-27 13:51:11 +0300

    Update Structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,5 @@
-import EventCard from "../components/EventCard/EventCard";
+import { redirect } from "next/navigation";
 
-const events = [
-  {
-    id: 1,
-    title: "Concert",
-    date: "2026-04-28",
-    time: "10:00",
-    location: "Antananarivo",
-  },
-  {
-    id: 2,
-    title: "Conférence Tech",
-    date: "2026-04-29",
-    time: "14:00",
-    location: "Ivandry",
-  },
-  {
-    id: 3,
-    title: "Meetup Dev",
-    date: "2026-04-30",
-    time: "16:00",
-    location: "Ankorondrano",
-  },
-];
-
-export default function EventsPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Événements</h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {events.map((event) => (
-          <EventCard
-            key={event.id}
-            title={event.title}
-            date={event.date}
-            time={event.time}
-            location={event.location}
-          />
-        ))}
-      </div>
-    </div>
-  );
+export default function Page() {
+  redirect("/events");
 }
