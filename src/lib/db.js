adb3ba31dde0bf716ca6628aa2425a60a251068b
tsiory <hei.tsiory.234@gmail.com>
2026-04-28 12:59:04 +0300

    Implemetation de login

---
+++ src/lib/db.js
@@ -0,0 +1,16 @@
+import { Pool } from "pg";
+
+let pool;
+
+export function getDb() {
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : false,
+    });
+  }
+  return pool;
+}
